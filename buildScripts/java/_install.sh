20152854f49b56de36fb73d2ea182b2fcfa36f50
yxc023 <yxc023@qq.com>
2017-07-20 16:38:23 +0800

    add

---
+++ buildScripts/java/_install.sh
@@ -0,0 +1,7 @@
+gradle/wrapper/gradle-wrapper.jar
+gradle/wrapper/gradle-wrapper.properties
+.gitignore
+build.gradle
+gradlew
+gradlew.bat
+
--- tools/install.sh
+++ tools/install.sh
@@ -1,3 +1,3 @@
-
-echo $1
-curl -fsSL https://github.com/robbyrussell/oh-my-zsh/blob/master/tools/install.sh > install.sh
+templateName=$0
+echo "https://github.com/robbyrussell/oh-my-zsh/blob/master/buildScriptes/"$templateName"/_install.sh"
+curl -fsSL "https://github.com/robbyrussell/oh-my-zsh/blob/master/buildScriptes/"$templateName"/_install.sh"
