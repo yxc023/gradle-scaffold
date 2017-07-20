de82d7e4e1379d5cbdfcddbeb1354d4c06e3b0bd
yxc023 <yxc023@qq.com>
2017-07-20 17:41:03 +0800

    up

--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,4 @@
 templateName=$0
 url="https://raw.githubusercontent.com/yxc023/spring-boot-scaffold/master/buildScripts/$templateName/_install.sh"
 echo $url
-exec "$(curl -fsSL $url)"
+sh -c "$(curl -fsSL $url)"
