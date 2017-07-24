4cb15505f361e33e5cedbe464d94ecc8d3e0638c
yxc023 <yxc023@qq.com>
2017-07-24 12:01:26 +0800

    add

---
+++ buildScripts/java-lianjia/_install.sh
@@ -0,0 +1,22 @@
+base_url="https://raw.githubusercontent.com/yxc023/spring-boot-scaffold/master/buildScripts/java/"
+
+file_array=(
+"gradle/wrapper/gradle-wrapper.jar" "gradle/wrapper/gradle-wrapper.properties"
+".gitignore"
+"build.gradle"
+"gradlew"
+"gradlew.bat"
+)
+
+mkdir -p gradle/wrapper
+
+for var in ${file_array[@]}
+do
+    echo $base_url$var
+    curl -fsSL $base_url$var > $var
+done
+
+chmod +x gradlew gradlew.bat
+
+echo "need 『fnrd.group.url』，『fnrd.group.username』，『fnrd.group.password』in file 『~/..gradle/gradle.properties』"
+echo "you can change the properites name also, just keep same with it in 『build.gradle』"
--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,15 @@
+if [ ! -n "$0" ] ;then
+    echo "Please input a templateName!"
+    exit
+fi
+
+if [ "$0" == "sh" ] ;then
+    echo "Please input a templateName!!"
+    exit
+fi
+
 templateName=$0
+
 url="https://raw.githubusercontent.com/yxc023/spring-boot-scaffold/master/buildScripts/$templateName/_install.sh"
 echo $url
 sh -c "$(curl -fsSL $url)"
