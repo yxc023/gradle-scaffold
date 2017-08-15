8fa32f7e01d032950623774629a4149d0ffb2bc1
yxc023 <yxc023@qq.com>
2017-08-15 17:53:44 +0800

    add minibuild

---
+++ install.sh
@@ -0,0 +1,19 @@
+base_url="https://raw.githubusercontent.com/yxc023/spring-boot-scaffold/master/miniBuild/"
+
+file_array=(
+"gradle/wrapper/gradle-wrapper.jar" "gradle/wrapper/gradle-wrapper.properties"
+".gitignore"
+"build.gradle"
+"gradlew"
+"gradlew.bat"
+)
+
+mkdir -p gradle/wrapper
+
+for var in ${file_array[@]}
+do
+    echo $base_url$var
+    curl -fsSL $base_url$var > $var
+done
+
+chmod +x gradlew gradlew.bat
